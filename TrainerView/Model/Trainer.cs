218d545e990dd76e848dfaa4b9d3f2673d889e3e
MaksimMurzin <maks_murzin@yahoo.com>
2024-03-02 22:50:50 +0000

    app works

---
+++ TrainerView/Model/Trainer.cs
@@ -0,0 +1,30 @@
+﻿namespace TrainerView.Model
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+        public Course Course { get; set; }
+        public int MasterScheduleID {  get; set; }
+        public string Reference { get; set; }
+        public string CourseName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int DayCount { get; set; }
+        public Status Status { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+    }
+
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Reference { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TrainerView/Services/ApiClientService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
+using TrainerView.Model;
+
+namespace TrainerView.Services
+{
+    public class ApiClientService
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _configuration;
+
+        public ApiClientService(IHttpClientFactory factory, IConfiguration configuration)
+        {
+            _client = factory.CreateClient();
+            _configuration = configuration;
+        }
+
+        public async Task<List<Trainer>> GetTrainersAsync()
+        {
+            var baseUri = _configuration.GetSection("BaseUri").Value;
+            var apiKey = _configuration.GetSection("ApiKey").Value;
+
+
+            return await _client.GetFromJsonAsync<List<Trainer>>(BuildUri(baseUri, apiKey));
+        }
+
+        private Uri BuildUri(string baseUri, string apiKey) 
+            => new Uri(baseUri.Replace("{ApiKey}", apiKey));
+
+    }
+}
